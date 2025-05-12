b88b7722fc0061de24e7c342b22b9878d351ce8e
Muhire James <mujames008@gmail.com>
2025-05-12 21:03:53 +0200

    edited sql to be able to order

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,7 +10,8 @@ SELECT
     u.last_name,
     u.email
 FROM Booking b
-INNER JOIN User u ON b.user_id = u.user_id;
+INNER JOIN User u ON b.user_id = u.user_id
+ORDER BY b.start_date DESC;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT 
@@ -20,7 +21,8 @@ SELECT
     r.rating,
     r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.property_id, r.rating DESC;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if not linked
 -- Note: This is supported in PostgreSQL. For MySQL, use the UNION workaround below.
@@ -33,7 +35,8 @@ SELECT
     b.start_date,
     b.end_date
 FROM User u
-FULL OUTER JOIN Booking b ON u.user_id = b.user_id;
+FULL OUTER JOIN Booking b ON u.user_id = b.user_id
+ORDER BY u.user_id;
 
 -- MySQL Workaround for FULL OUTER JOIN using UNION
 SELECT 
@@ -58,4 +61,5 @@ SELECT
     b.start_date,
     b.end_date
 FROM User u
-RIGHT JOIN Booking b ON u.user_id = b.user_id;
+RIGHT JOIN Booking b ON u.user_id = b.user_id
+ORDER BY user_id;
