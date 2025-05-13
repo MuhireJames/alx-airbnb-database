7de9b5ba09f80976953819573e4c1612b3e8d8c4
MUHIRE James <105197491+MuhireJames@users.noreply.github.com>
2025-05-13 19:46:16 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,62 +4,63 @@ SELECT
     Booking.property_id,
     Booking.start_date,
     Booking.end_date,
-    Booking.status,
+    Booking.total_price,
+    User.user_id,
     User.first_name,
     User.last_name,
     User.email
-FROM Booking
-INNER JOIN User ON User.user_id = Booking.user_id
+FROM Booking 
+INNER JOIN User  ON Booking.user_id = User.user_id
 ORDER BY Booking.start_date DESC;
 
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT 
-    p.property_id,
-    p.name AS property_name,
-    r.review_id,
-    r.rating,
-    r.comment
-FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id
-ORDER BY p.property_id, r.rating DESC;
+    Property.property_id,
+    Property.name AS property_name,
+    Review.review_id,
+    Review.rating,
+    Review.comment
+FROM Property
+LEFT JOIN Review  ON Property.property_id = Review.property_id
+ORDER BY Property.property_id, Review.rating DESC;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if not linked
 -- Note: This is supported in PostgreSQL. For MySQL, use the UNION workaround below.
 SELECT 
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.property_id,
-    b.start_date,
-    b.end_date
-FROM User u
-FULL OUTER JOIN Booking b ON u.user_id = b.user_id
-ORDER BY u.user_id;
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date
+FROM User 
+FULL OUTER JOIN Booking  ON User.user_id = Booking.user_id
+ORDER BY User.user_id;
 
 -- MySQL Workaround for FULL OUTER JOIN using UNION
 SELECT 
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.property_id,
-    b.start_date,
-    b.end_date
-FROM User u
-LEFT JOIN Booking b ON u.user_id = b.user_id
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date
+FROM User
+LEFT JOIN Booking  ON User.user_id = Booking.user_id
 
 UNION
 
 SELECT 
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.property_id,
-    b.start_date,
-    b.end_date
-FROM User u
-RIGHT JOIN Booking b ON u.user_id = b.user_id
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date
+FROM User
+RIGHT JOIN Booking  ON User.user_id = Booking.user_id
 ORDER BY user_id;
